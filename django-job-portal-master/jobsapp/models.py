8fae08c766ecdd0e5b8a071b99dfeb128fd84d3d
Arora <ARORAAA1@novartis.net>
2020-12-11 13:53:21 +0530

    Update Job apply form + error handling

--- django-job-portal-master/jobsapp/models.py
+++ django-job-portal-master/jobsapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django import forms
+from django.core.validators import RegexValidator
 
 from accounts.models import User
 
@@ -10,6 +11,10 @@ JOB_TYPE = (
     ('3', "Internship"),
 )
 
+TRUE_FALSE_CHOICES = (
+    ('1', 'Yes'),
+    ('2', 'No')
+)
 
 class Job(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,20 +36,34 @@ class Job(models.Model):
 
 class UserDetials(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applicant')
-    firstname = models.CharField(max_length=300)
-    lastname = models.CharField(max_length=300)
+    fullname = models.CharField(max_length=300,default='null')
 
-    # email = models.EmailField(unique=True, blank=False,
-    #                           error_messages={
-    #                               'unique': "A user with that email already exists.",
-    #                           })
+    email = models.EmailField(blank=False, default='null', unique=True,
+                              error_messages={
+                                  'unique': "A user with that email already exists.",
+                              })
+    phone_regex = RegexValidator(regex=r'^\d{10}$', message="Phone number must be a 10 digit number")
+    phone_number = models.CharField(validators=[phone_regex], max_length=10, unique=True,
+                                    error_messages={
+                                        'unique': "A user with that phone number already exists.",
+                                    }) # validators should be a list
     resume = models.FileField()
-    date_of_birth = models.DateField(blank=True, null=True)
-    degree = models.CharField(max_length=300)
-    institute = models.CharField(max_length=300)
-    department = models.CharField(max_length=300)
+    date_of_birth = models.DateField()
+    highest_qualification = models.TextField(default='null')
+    total_experience_yrs = models.IntegerField(default=0)
+    total_experience_mos = models.IntegerField(default=0)
+    ngo_experience_yrs = models.IntegerField(default=0)
+    ngo_experience_mos = models.IntegerField(default=0)
+    skill_set = models.TextField(default='null')
+    current_ctc = models.IntegerField(default=0)
+    expected_ctc = models.IntegerField(default=0)
+    notice_period = models.IntegerField(default=0)
+    current_org = models.CharField(max_length=300,default='null')
+    current_designation = models.CharField(max_length=300,default='null')
+    current_city = models.CharField(max_length=300,default='null')
+    relocate = models.CharField(choices=TRUE_FALSE_CHOICES, max_length=10,default='null')
     def __str__(self):
-        return self.firstname
+        return self.fullname
 
 class Applicant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
